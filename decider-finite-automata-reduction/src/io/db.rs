6b162591a0d164ea8fdd981421bcdb9f96769b8c
Justin Blanchard <UncombedCoconut@gmail.com>
2023-10-12 22:58:50 -0400

    Fix reading of seed-DB format for multi-symbol.

--- decider-finite-automata-reduction/src/io/db.rs
+++ decider-finite-automata-reduction/src/io/db.rs
@@ -1,14 +1,14 @@
 //! A seed database as in https://bbchallenge.org/method.
 
 use super::MachineID;
-use crate::core::{Machine, TM_STATES};
+use crate::core::{Machine, SYMBOLS, TM_STATES};
 use std::fs::File;
 use std::io::{self, BufReader, Read, Seek};
 use std::path::Path;
 use zerocopy::FromBytes;
 
 const HEADER_SIZE: i64 = 30;
-const RECORD_SIZE: i64 = 6 * TM_STATES as i64;
+const RECORD_SIZE: i64 = (3 * SYMBOLS * TM_STATES) as i64;
 
 pub struct Database {
     file: File,
@@ -27,7 +27,7 @@ impl Database {
     ) -> impl Iterator<Item = (MachineID, Machine)> + 'a {
         let mut reader = BufReader::new(&self.file);
         let pos = reader.stream_position().unwrap() as i64;
-        Reader { reader, ids, bytes: [0u8; 6 * TM_STATES], pos }
+        Reader { reader, ids, bytes: [0u8; 3 * SYMBOLS * TM_STATES], pos }
     }
 
     pub fn len(&self) -> usize {
@@ -39,7 +39,7 @@ impl Database {
 pub struct Reader<'a, I: Iterator<Item = MachineID> + 'a> {
     reader: BufReader<&'a File>,
     ids: I,
-    bytes: [u8; 6 * TM_STATES],
+    bytes: [u8; 3 * SYMBOLS * TM_STATES],
     pos: i64,
 }
 
